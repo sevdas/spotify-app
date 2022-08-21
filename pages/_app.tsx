a2c6ca69b3cafcaebba500764417a92523755e37
Sevda Shukrieva <sevda.shukrieva@xdesign.com>
2022-08-21 17:37:51 +0100

    add customs color tokens, cutomise focus state for Button component from the component library

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,40 @@
-import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+
+const customTheme = extendTheme({
+  colors: {
+    gray: {
+      100: "#F5F5F5",
+      200: "#EEEEEE",
+      300: "#E0E0E0",
+      400: "#BDBDBD",
+      500: "#9E9E9E",
+      600: "#757575",
+      700: "#616161",
+      800: "#424242",
+      900: "#212121",
+    },
+  },
+  components: {
+    Button: {
+      variants: {
+        link: {
+          ":focus": {
+            outline: "none",
+            boxShadow: "none",
+          },
+        },
+      },
+    },
+  },
+});
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />;
+  return (
+    <ChakraProvider theme={customTheme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 };
 
 export default MyApp;
