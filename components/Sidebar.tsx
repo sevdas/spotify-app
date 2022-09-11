3483b693c5633273c18f48ad13e105f6755b092e
Sevda Shukrieva <sevda.shukrieva@xdesign.com>
2022-09-11 23:35:06 +0100

    add logo :musical_score:

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,3 +1,45 @@
+import NextImage from "next/image";
+import {
+  List,
+  ListItem,
+  ListIcon,
+  Divider,
+  Center,
+  LinkBox,
+  LinkOverlay,
+  OrderedList,
+  UnorderedList,
+  Box,
+} from "@chakra-ui/react";
+import {
+  MdHome,
+  MdSearh,
+  MdLibraryMusic,
+  MdPlaylistAdd,
+  MdFavorite,
+} from "react-icons/md";
+
 export const Sidebar = () => {
-  return <div>Hello from sidebar</div>;
+  return (
+    <Box
+      sx={{
+        width: "100px",
+        height: "calc(100vh - 100px)",
+        bg: "black",
+        paddingX: "5px",
+        color: "gray",
+      }}
+    >
+      <Box sx={{ paddingY: "20px" }}>
+        <Box sx={{ marginBottom: "20px", paddingY: "20px" }}>
+          <NextImage
+            src="/sanfranciscomunicipalrailway.svg"
+            alt="Music player"
+            height={60}
+            width={120}
+          />
+        </Box>
+      </Box>
+    </Box>
+  );
 };
