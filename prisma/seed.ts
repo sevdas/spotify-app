efd65b32a74ddbbcb6df3c41bcc7010a4f76eddd
Sevda Shukrieva <sevda.shukrieva@xdesign.com>
2022-10-09 22:18:22 +0100

    seed the data

---
+++ prisma/seed.ts
@@ -0,0 +1,37 @@
+/* eslint-disable no-unused-vars */
+import { PrismaClient } from "@prisma/client";
+import bcrypy from "bcrypt";
+import { url } from "inspector";
+import { artistsData } from "./songsData";
+
+// Handle the database connection
+const prisma = new PrismaClient();
+
+const run = async () => {
+  await Promise.all(
+    artistsData.map(async (artist) => {
+      return prisma.artist.upsert({
+        where: { name: artist.name },
+        update: {},
+        create: {
+          name: artist.name,
+          songs: {
+            create: artist.songs.map((song) => ({
+              name: song.name,
+              duration: song.duration,
+              url: song.url,
+            })),
+          },
+        },
+      });
+    })
+  );
+};
+
+run()
+  .catch((e) => {
+    // eslint-disable-next-line no-console
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(() => prisma.$disconnect);
